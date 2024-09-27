d30ec0b2851c1f6d6b9a9919b4d5b1b3e485f62e
yzs <2627689178@qq.com>
2024-09-27 15:23:18 +0800

    key file

--- src/main/java/show/ywy/MonkeySpaceApplication.java
+++ src/main/java/show/ywy/MonkeySpaceApplication.java
@@ -1,13 +1,15 @@
 package show.ywy;
 
-import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import show.ywy.entity.SecureTool;
 
 @SpringBootApplication
 public class MonkeySpaceApplication {
 
     public static void main(String[] args) {
+        // create local key
+        SecureTool.createKey();
         SpringApplication.run(MonkeySpaceApplication.class, args);
     }
 
--- src/main/java/show/ywy/entity/SecureTool.java
+++ src/main/java/show/ywy/entity/SecureTool.java
@@ -1,10 +1,15 @@
 package show.ywy.entity;
 
 import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -41,4 +46,25 @@ public class SecureTool {
         return SecureUtil.rsa(PRIVATE_KEY, PUBLIC_KEY).decryptStr(data, KeyType.PrivateKey);
     }
 
+    public static void createKey() {
+        try (HttpResponse execute = HttpUtil.createPost("http://web.chacuo.net/netrsakeypair")
+                .form("data", "2048")
+                .form("type", "rsakeypair")
+                .form("arg", "t%3D1_o%3D0_p%3D")
+                .header("Accept", "*/*")
+                .header("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
+                .header("proxy-connection", "keep-alive")
+                .header("x-requested-with", "XMLHttpRequest")
+                .header("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+                .execute()) {
+            String body = execute.body();
+            System.out.println(body);
+            String public_key = JSONUtil.parse(body).getByPath("data[0][0]").toString();
+            String file = ResourceUtil.getResource("key/public.key").getFile();
+            FileWriter.create(new File(file)).write(public_key, false);
+            String private_key = JSONUtil.parse(body).getByPath("data[0][1]").toString();
+            String file2 = ResourceUtil.getResource("key/private.key").getFile();
+            FileWriter.create(new File(file2)).write(private_key, false);
+        }
+    }
 }
